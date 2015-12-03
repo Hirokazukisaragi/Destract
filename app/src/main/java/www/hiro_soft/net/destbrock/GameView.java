7cf6586428753fd5e3dadf74a6e08b073a6ec6f8
hirokazu <hiro@hiro-soft.net>
2015-12-03 13:09:17 +0900

    Apply GPLv3

--- app/src/main/java/www/hiro_soft/net/destbrock/GameView.java
+++ app/src/main/java/www/hiro_soft/net/destbrock/GameView.java
@@ -14,6 +14,27 @@ import java.util.TimerTask;
 /**
  * Created by hirokazu on 15/12/01.
  */
+
+/*import android.support.v7.app.AppCompatActivity;
+
+Copyright (C) 2015  Seno Hirokazu
+
+This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation; either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+*/
+
+
 public class GameView extends View{
     private Paint paint = new Paint();
    // private Block block = new Block;
